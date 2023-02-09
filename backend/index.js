41897f0eb686fd1639a9fbfc4891e64f9dd9365b
fergus1243 <f.macfarlane@rgu.ac.uk>
2023-02-09 14:46:59 +0000

    added db

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,26 @@ var path=require('path');
 var express =require('express');
 var app=express();
 
+const{MongoClient} = require("mongodb");
+const url = "mongodb://127.00.1:27017";
+
+const client = new MongoClient(url);
+async function run(){
+    try{
+        var dbo = client.db("mydb");
+        var myobj = {quotename: n, salary:s , days: d};
+        await dbo.collection("quotes").insertOne(myobj, function(err, res){
+            if(err){
+                console.log(err);
+                throw err;
+            }
+            console.log("1 quote inserted");
+        });
+    }finally{
+        await client.close();
+    }
+}
+run().catch(console.dir);
 var option = {
     index:"myWebPage.html"
 };
